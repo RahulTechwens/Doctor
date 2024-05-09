0386a14582632c46ff5b238753ce5946e006e429
techwens <rahul.sabui@techwens.in>
2024-05-09 12:30:30 +0530

    working on sequelize

--- app/Module/Report/report.controller.js
+++ app/Module/Report/report.controller.js
@@ -5,9 +5,24 @@ exports.transaction = async (req, res, next) => {
     const from_date_str = req?.query?.from_date;
     const to_date_str = req?.query?.to_date;
     const filter = req?.query?.filter;
-
+    let extractArr = []
     const reportTrans = await  transactionReport(from_date_str, to_date_str, filter);
     if (reportTrans) {
+
+      for (let index = 0; reportTrans < array.length; index++) {
+        const reportTransElement = reportTrans[index];
+        extractArr.push(
+          {
+            amount:reportTransElement?.amount,
+            total_amount:reportTransElement?.total_amount,
+            full_name:reportTransElement?.User?.UserProfiles?.full_name,
+            address:reportTransElement?.User?.UserProfiles?.address,
+            phone:reportTransElement?.User?.phone,
+            email:reportTransElement?.User?.email,
+          }
+        )
+      }
+
       return res.status(200).json({
         status: 200,
         success: true,
