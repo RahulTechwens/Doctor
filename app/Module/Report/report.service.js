ead4d33ff0afe82f2b41282d5caafd46ed9a985f
techwens <rahul.sabui@techwens.in>
2024-05-13 13:32:28 +0530

    working on patient report

--- app/Module/Report/report.service.js
+++ app/Module/Report/report.service.js
@@ -1,5 +1,11 @@
 const { Op } = require("sequelize");
-const { slot_money, User, UserProfile, slot_book, slot_entries } = require("../../../models/");
+const {
+  slot_money,
+  User,
+  UserProfile,
+  slot_book,
+  slot_entries,
+} = require("../../../models/");
 const moment = require("moment");
 
 exports.transactionReport = async (from_date, to_date, filter) => {
@@ -72,7 +78,6 @@ exports.transactionReport = async (from_date, to_date, filter) => {
 exports.patientBookngReport = async (from_date, to_date, filter) => {
   if (filter == "custom") {
     if (from_date && to_date) {
-      
       const patient_bookng_report_model_custom = await slot_book.findAll({
         where: {
           date: {
@@ -92,17 +97,54 @@ exports.patientBookngReport = async (from_date, to_date, filter) => {
             ],
           },
         ],
-        raw:true,
-        nest:true
-        
-      })
-      
-
+        raw: true,
+        nest: true,
+      });
       return patient_bookng_report_model_custom;
     }
   }
   if (filter == "today") {
+    let todays_date = moment().format("YYYY-MM-DD");
+    const patient_bookng_report_model_today = await slot_book.findAll({
+      where: {
+        date: todays_date,
+      },
+      include: [
+        {
+          model: slot_entries,
+        },
+        {
+          model: User,
+          include: [
+            {
+              model: UserProfile,
+            },
+          ],
+        },
+      ],
+      raw: true,
+      nest: true,
+    });
+    return patient_bookng_report_model_today;
   }
   if (filter == "all") {
+    const patient_bookng_report_model_all = await slot_book.findAll({
+      include: [
+        {
+          model: slot_entries,
+        },
+        {
+          model: User,
+          include: [
+            {
+              model: UserProfile,
+            },
+          ],
+        },
+      ],
+      raw: true,
+      nest: true,
+    });
+    return patient_bookng_report_model_all;
   }
 };
