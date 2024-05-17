580da2df452f0c7aea89794686e4983437d0084f
techwens <rahul.sabui@techwens.in>
2024-05-17 17:07:11 +0530

    wokring on pagination

--- app/Utils/pagination.js
+++ app/Utils/pagination.js
@@ -1,6 +1,6 @@
 exports.getPagination = (page, size) => {
-  const limit = size;
+  const limit = size ? parseInt(size, 10) : 10;
   const parsedPage = parseInt(page, 10);
-  const offset = parsedPage ? (parsedPage - 1) * limit : 0;
+  const offset = parsedPage && parsedPage > 0 ? (parsedPage - 1) * limit : 0;
   return { limit, offset };
 };
