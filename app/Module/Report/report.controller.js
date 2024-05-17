6a49278289b6754856c7a3268441f9628ac2a1b9
techwens <rahul.sabui@techwens.in>
2024-05-17 16:52:45 +0530

    wokring on pagination

--- app/Module/Report/report.controller.js
+++ app/Module/Report/report.controller.js
@@ -10,9 +10,9 @@ exports.transaction = async (req, res, next) => {
     let extractArr = []
     const reportTrans = await  transactionReport(from_date_str, to_date_str, filter, offset, limit);
     if (reportTrans) {
-      console.log(reportTrans);
       for (let index = 0; index < reportTrans.rows.length; index++) {
         const reportTransElement = reportTrans[index];
+        console.log(reportTransElement);
         extractArr.push(
           {
             id:reportTransElement?.id,
