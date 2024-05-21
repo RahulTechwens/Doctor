86e5c9b5dccbb7b1b9e8d585a283a9a1f4f57b2f
techwens <rahul.sabui@techwens.in>
2024-05-21 16:41:48 +0530

    working on reschudule

--- app/Module/SlotBook/slotbook.controller.js
+++ app/Module/SlotBook/slotbook.controller.js
@@ -29,7 +29,6 @@ exports.bookSlot = async (req, res, next) =>{
                 return handleErrorMessage(res, 400, "Slots are full.");
             }
         }else{
-            console.log(checkSlotEntry?);
             delete payloadOfSlotBook.type
             if (checkSlot.length < checkSlotEntry?.length) {
                 const isReschedule = await reschedule(checkSlotEntry, payloadOfSlotBook)
